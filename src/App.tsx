5025b49a199fc0f43fc233173451b0c84f5d36e2
devTag59 <fernandoKambambi40@gmail.com>
2026-02-24 22:01:20 +0100

    adicionando o action para pesquisar com enter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import axios from "axios"
-import './App.css'
 
 function App() {
   const [Tempo, setTempo] = useState("")
   const API_KEY='481390f99d35ba6cce4bb6670ce28239'
   const[cidade,setCidade]=useState('')
   const[onLoading,setLoading]=useState(false)
+  const handleKeyPress =(event: { key: string })=>{
+    if(event.key==='Enter'){
+      getClima()
+    }
+  }
+
   const getClima=async()=>{
     try{
       if(!cidade) return;
@@ -31,11 +36,11 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='bg-blue-300 h-screen flex flex-col items-center justify-center gap-4'>
         <p>Hello, this is the Find_clima-web app!</p>
         <p>{Tempo}</p>
         <p>Digite o nome da sua cidade</p>
-        <input type="text" placeholder='Digite o nome da cidade' value={cidade} onChange={(e) => setCidade(e.target.value)}/>
+        <input type="text" placeholder='Digite o nome da cidade' value={cidade} onKeyPress={handleKeyPress} onChange={(e) => setCidade(e.target.value)}/>
         <button onClick={getClima} disabled={onLoading} className='bg-blue-600'>Buscar Clima</button>
       </div>
     </>
