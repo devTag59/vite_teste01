4f3b8c9c43c1a57a79d0c2bdfe00f24350194d8c
devTag59 <fernandoKambambi40@gmail.com>
2026-02-24 23:27:53 +0100

    adicionando imagem e primeira interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { useState } from 'react'
 import axios from "axios"
 
+interface WeatherData {
+  description: string;
+  [key: string]: any;
+}
+
 function App() {
   const [Tempo, setTempo] = useState("")
+  const [weather, setWeather] = useState<WeatherData[]>([])
+  const [iconUrl,setIconUrl]=useState("")
   const API_KEY='481390f99d35ba6cce4bb6670ce28239'
   const[cidade,setCidade]=useState('')
   const[onLoading,setLoading]=useState(false)
@@ -22,7 +29,10 @@ function App() {
       units:'metric'
     }
     });
-    console.log(response.data.main)
+    console.log(response.data)
+    console.log(response.data.weather[0].description)
+    setIconUrl(`http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`)
+    setWeather(response.data.weather)
     setTempo(response.data.main.temp)
     }
     catch(error){
@@ -36,12 +46,27 @@ function App() {
 
   return (
     <>
-      <div className='bg-blue-300 h-screen flex flex-col items-center justify-center gap-4'>
-        <p>Hello, this is the Find_clima-web app!</p>
+      <div className='bg-gray-800 h-screen flex flex-col items-center justify-center gap-4'>
+        <p className='text-white font-bold text-5xl'>BEM VINDO AO FIND-CLIMA</p>
+        <div style={{color:'white', font:"bold"}} className='w-72 h-72 flex flex-col items-center justify-evenly bg-gray-600 mb-4 rounded-3xl'>
         <p>{Tempo}</p>
+        <p>{weather.length > 0 ? weather[0].description : "Sem dados de clima"}</p>
+        <img src={iconUrl}/>
         <p>Digite o nome da sua cidade</p>
-        <input type="text" placeholder='Digite o nome da cidade' value={cidade} onKeyPress={handleKeyPress} onChange={(e) => setCidade(e.target.value)}/>
-        <button onClick={getClima} disabled={onLoading} className='bg-blue-600'>Buscar Clima</button>
+        <input className=' outline-none
+        border-2 
+        border-gray-300 
+        rounded-lg 
+        p-3 
+        w-64
+        focus:border-blue-500
+        focus:ring-2
+        focus:ring-blue-200
+        focus:ring-opacity-50
+        transition-all
+        duration-200' type="text" placeholder='Digite o nome da cidade' value={cidade} onKeyPress={handleKeyPress } onChange={(e) => setCidade(e.target.value)}/>
+        <button className='bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={getClima} disabled={onLoading}>Buscar Clima</button>
+        </div>
       </div>
     </>
   )
