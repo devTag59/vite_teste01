56df064865a0d3a4f24d42e17bcae725e562ab0e
devTag59 <fernandoKambambi40@gmail.com>
2026-03-03 17:35:47 -0300

    botao com logica condicional

--- src/App.tsx
+++ src/App.tsx
@@ -64,8 +64,12 @@ function App() {
         focus:ring-blue-200
         focus:ring-opacity-50
         transition-all
-        duration-200' type="text" placeholder='Digite o nome da cidade' value={cidade} onKeyPress={handleKeyPress } onChange={(e) => setCidade(e.target.value)}/>
-        <button className='bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={getClima} disabled={onLoading}>Buscar Clima</button>
+        duration-200' type="text"
+        placeholder='Digite o nome da cidade'
+        value={cidade} onKeyPress={handleKeyPress }
+        onChange={(e) => setCidade(e.target.value)}/>
+        <button className='bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+        onClick={getClima} disabled={onLoading}>{onLoading?"Carregando":"Clique para pesquisar"}</button>
         </div>
       </div>
     </>
