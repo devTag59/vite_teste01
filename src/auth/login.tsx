5f0fb62d88ef06cc6cf21d789f81f5fe4f166168
devTag59 <fernandoKambambi40@gmail.com>
2026-03-07 20:50:26 -0300

    redesenho da pagina de login

--- src/auth/login.tsx
+++ src/auth/login.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { Link, Route, Router, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 interface User {
   nome: string;
@@ -36,30 +36,36 @@ function Login() {
     }
   }
   return (
-    <div className='bg-gray-200 h-screen flex flex-col items-center justify-center gap-4'>
-        <p className='text-white font-bold text-5xl'>BEM VINDO AO FIND-CLIMA</p>
-        <div style={{color:'white', font:"bold"}} className='w-72 h-72 flex flex-col items-center justify-evenly bg-gray-600 mb-4 rounded-3xl'> 
-        <p>Informe o seu email</p>
+    <div className='bg-gray-900 h-screen flex items-center justify-center gap-4'>
+      <div className='h-full w-full flex-col content-center items-center gap-4 justify-center text-center'>
+            <p className='text-white font-bold text-5xl'>BEM VINDO AO FIND-CLIMA</p>
+            <p className='text-white'>Informe seu email e senha para continuar</p>
+        </div>
+        <div className='h-full w-full inline-flex items-center justify-center'>
+          <div style={{color:'white', font:"bold"}} className='w-1/2 h-1/2 flex flex-col items-center justify-evenly bg-gray-800 mb-4 rounded-3xl'> 
+        <p className='text-5xl'>Informe o seu email</p>
         <input className=' outline-none
         border-2 
         border-gray-300 
         rounded-lg 
         p-3
-        w-64'
+        w-3/4'
         value={nome}
         onChange={(e)=>setNome(e.target.value)}
         type="text" placeholder='Digite sua cidade'/>
-        </div>
-        
-        <button className='bg-blue-500
+          <button className='bg-blue-500
                         text-white
                         font-bold
+                        w-3/4
                         py-2 px-4
                         rounded-lg
                         hover:bg-blue-600
                         transition-colors
-                        duration-300' onClick={handleLogin}>Entrar</button>
+                        duration-300' onClick={handleLogin}>Entrar
+          </button>
         
+        </div>
+      </div>   
     </div>
   )
 }
