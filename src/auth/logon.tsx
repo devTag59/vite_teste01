a2abbd1e00cf9a419dbc95d1c86c4786aec632a6
devTag59 <fernandoKambambi40@gmail.com>
2026-03-12 12:07:00 +0100

    estilizando o login

--- src/auth/logon.tsx
+++ src/auth/logon.tsx
@@ -18,10 +18,26 @@ function Logon() {
     }
     
 }
-    return(<div>
+    return(<>
+    <div className="w-full 
+    lg:w-1/2              {/* lg: metade da tela */}
+    flex 
+    flex-col 
+    items-center 
+    lg:items-start       {/* lg: alinhado à esquerda */}
+    justify-center 
+    text-center 
+    lg:text-left         {/* lg: texto à esquerda */}
+    p-6
+    space-y-4">
+    <div>
+        <text style={{color:"white", font:"bold"}}>Registrar novo usuário</text>
+        <text style={{color:"white", font:"bold"}}>Digite seu nome e senha para criar uma conta</text>
+    </div>
         <input type="text" placeholder="Nome" value={nome} onChange={(e)=>setNome(e.target.value)}/>
         <input type="password" placeholder="Senha" value={senha} onChange={(e)=>setSenha(e.target.value)}/>
         <button onClick={setUsers}>Registrar</button>
-    </div>)
+        </div>
+    </>)
 }
 export default Logon
